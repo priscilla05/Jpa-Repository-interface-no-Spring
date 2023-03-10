2de793af0a1d46d96ad07920176532d44cfb9673
PriscillaCorreia <primariafc@gmail.com>
2023-03-10 13:33:57 -0300

    aula de jpa repository com Spring

---
+++ src/main/java/com/projetos/java/aulajparepository/AulaJpaRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.projetos.java.aulajparepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaJpaRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaJpaRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetos/java/aulajparepository/AulaJpaRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.java.aulajparepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaJpaRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
